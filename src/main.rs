80b563a18cff7fc7ee38a690ae70bb56b9969e18
George <georgiyozhegov.rs@gmail.com>
2024-06-06 14:17:03 +0400

    Add: instructions generatation from AST

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use qoolang::lex::lex;
 use qoolang::parse::parse;
+use qoolang::generate::*;
 
 fn main()
 {
-        let source = "mutable a : !12";
+        let source = "a : 12 b : 3 c : (a + b) * 2";
         let source = source.chars().peekable();
         let tokens = lex(source);
         let tokens = tokens.into_iter().peekable();
         let tree = parse(tokens);
-        println!("{tree:?}");
+        let instructions = generate(tree);
+        println!("{instructions:?}");
 }
